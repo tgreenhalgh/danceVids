df889ca35f6085828aed395682a414ad39340f2b
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2017-03-12 17:28:58 -0700

    adds webpack watch

--- public/javascript/MyTitle.js
+++ public/javascript/MyTitle.js
@@ -4,15 +4,24 @@
 // ES6 modules way
 import React from 'react';
 
-
 // create the `stamp`
 var div = React.DOM.div;
 var h1 = React.DOM.h1;
 
+// note: everything to do with MyTitle is here
+// so makes for easier debugging if there's an error
+// with MyTitle
 var MyTitle = React.createClass({
   render: function() {
+    //added for jxs, not there for javascript
+    const style = {
+      color: this.props.color,
+      fontWeight: 'bold'
+    };
     return (
-      div(null,
+      // javascript version:
+      /*
+       div(null,
         // `this` is the element we created
         // `props` is readonly from the parent
         // `title` is property name used
@@ -20,7 +29,18 @@ var MyTitle = React.createClass({
           color: this.props.color,
           fontWeight: 'bold'
         } }, this.props.title)
-      )
+       )
+      */
+      // jsx
+      // note: { } interpolates what's in them,
+      // i.e. is a javascript expression
+      // anything else just shows up
+      <div>
+        <h1 style= { style }>
+          { this.props.title.toUpperCase() }
+        }
+        </h1>
+      </div>
     );
   }
 });
